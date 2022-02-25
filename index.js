35ca50b838541368e1622a5d306757405cd8b24f
Rebecca Cohen <rcohen2@ufl.edu>
2022-02-25 12:37:12 -0500

    array created

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // Create an Array of at least 3 losing messages
+var lose = [
+  'Try again', //0
+  'Maybe next time', //1
+  'Sorry not this time'] //2
 
 // Create variables to count wins and losses
 
+
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+
 
 // target all .box elements and attach a click event listener to each one using a loop
 
+
+
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
 
+
+// convert that value to a Number and store it to a variable
 // create a random number between 1-3 and store it to a variable
+
+
 // This number will represent the winning box
 
 // determine if the box clicked is equal to the random number
+
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
 
